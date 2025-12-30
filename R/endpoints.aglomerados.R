a6fa0159c65ab7fca2cc76076aeffabba4046724
AlenCarbajal <alencarbajal@gmail.com>
2025-12-30 11:38:16 -0300

    devtools namespaces

--- R/endpoints.aglomerados.R
+++ R/endpoints.aglomerados.R
@@ -1,3 +1,4 @@
+#' Obtener aglomerados
 get_aglomerados <- function(
     id = NULL,
     nombre = NULL,
@@ -12,8 +13,8 @@ get_aglomerados <- function(
     get_endpoint(endpoint = "aglomerados", args = as.list(environment()))
 }
 
-
-#' @return devuelve un tibble con los resultados para cada query o 
+#' Obtener aglomerados en batch
+#' @return devuelve un tibble con los resultados para cada query o
 #' @export
 post_aglomerados <- function(queries_list){
   bulk_post_request(endpoint = "aglomerados", queries_list = queries_list)
